88e3a487fe6dc5fea78592ba45bdfa467e72bbd9
Rdo <gabrielcarriel44@gmail.com>
2026-08-24 21:12:44 +0000

    organização do codigo

--- api.js
+++ api.js
@@ -1,36 +1,36 @@
 import http from 'node:http';
 
-
 const porta = 3000;
 const tarefas = [
-    {id: 1, titulo: 'Resenhar'},
-    {id: 2, titulo: 'Farmar Aura'}
+    { id: 1, titulo: 'Resenhar' },
+    { id: 2, titulo: 'Farmar Aura' }
 ];
 
-
 const server = http.createServer((req, res) => {
-    res.setHeader('content-type', 'application/json; charset=utf-8')
-
+    res.setHeader('content-type', 'application/json; charset=utf-8');
 
     if (req.method === 'GET' && req.url === '/tarefas') {
         res.statusCode = 200;
         res.end(JSON.stringify(tarefas));
     } else if (req.method === 'POST' && req.url === '/tarefa') {
         let body = '';
+        
         req.on('data', (chunk) => {
             body += chunk.toString();
-        
         });
+        
         req.on('end', () => {
-        try {
-            const novaTarefa = JSON.parse(body);
-        
-        
-        }   catch (error) {
-            
-        }
+            try {
+                const novaTarefa = JSON.parse(body);
+                if (!novaTarefa.titulo) {
+                    res.statusCode = 400;
+                    res.end(JSON.stringify({ error: 'O campo "titulo" é obrigatório.' }));
+                    return;
+                }
+            } catch (error) {
 
-        })
+            }
+        });
     }
 });
 
